92657e336d2c8a0b4943b7e0786da97f467c6859
MalikaSadullaeva96 <malika@Malikas-MacBook-Pro.local>
2023-06-05 11:37:11 +0500

    feat: extract name

--- src/01-strings-tasks.js
+++ src/01-strings-tasks.js
@@ -65,8 +65,10 @@ function getStringFromTemplate(firstName, lastName) {
  *   'Hello, John Doe!' => 'John Doe'
  *   'Hello, Chuck Norris!' => 'Chuck Norris'
  */
-function extractNameFromTemplate(/* value */) {
-  throw new Error('Not implemented');
+function extractNameFromTemplate(value) {
+  const startIndex = value.indexOf(' ') + 1;
+  const endIndex = value.lastIndexOf('!');
+  return value.substring(startIndex, endIndex);
 }
 
 
