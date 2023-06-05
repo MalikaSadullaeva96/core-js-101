486722a16785c7a9fb09445797a6f1b9b9cc5a4d
MalikaSadullaeva96 <malika@Malikas-MacBook-Pro.local>
2023-06-05 22:45:23 +0500

    feat: removeFalsyValues

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -109,8 +109,10 @@ function getArrayOfStrings(arr) {
  *    [ 1, 2, 3, 4, 5, 'false' ]         => [ 1, 2, 3, 4, 5, 'false' ]
  *    [ false, 0, NaN, '', undefined ]   => [ ]
  */
-function removeFalsyValues(/* arr */) {
-  throw new Error('Not implemented');
+function removeFalsyValues(arr) {
+  const exclude = [false, null, 0, undefined, '', NaN];
+  const answer = arr.filter((val) => !exclude.includes(val));
+  return answer;
 }
 
 /**
