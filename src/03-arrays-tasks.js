62c3ab0d353b175daafbbe268d986d1d8495fa32
MalikaSadullaeva96 <malika@Malikas-MacBook-Pro.local>
2023-06-05 22:25:28 +0500

    feat: getArrayOfPositives

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -53,8 +53,9 @@ function generateOdds(len) {
  *    [0, 1, 2, 3, 4, 5] => [0, 1, 2, 3, 4, 5,   0, 1, 2, 3, 4, 5]
  *    [] => []
  */
-function doubleArray(/* arr */) {
-  throw new Error('Not implemented');
+function doubleArray(arr) {
+  const arr2 = arr;
+  return arr.concat(arr2);
 }
 
 
@@ -69,8 +70,8 @@ function doubleArray(/* arr */) {
  *    [-1, 2, -5, -4, 0] => [ 2 ]
  *    [] => []
  */
-function getArrayOfPositives(/* arr */) {
-  throw new Error('Not implemented');
+function getArrayOfPositives(arr) {
+  return arr.filter((val) => val > 0);
 }
 
 /**
