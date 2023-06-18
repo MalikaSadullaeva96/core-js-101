806c20be938385fa4672c2bc8600fb9b9acb1aad
MalikaSadullaeva96 <m.sadullaeva96@gmail.com>
2023-06-18 23:45:23 +0500

    feat: function length of pos num

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -343,8 +343,9 @@ function get3TopItems(arr) {
  *   [ null, 1, 'elephant' ] => 1
  *   [ 1, '2' ] => 1
  */
-function getPositivesCount(/* arr */) {
-  throw new Error('Not implemented');
+function getPositivesCount(arr) {
+  const answer = arr.filter((val) => typeof val === 'number' && val > 0);
+  return answer.length;
 }
 
 /**
