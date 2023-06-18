7bb43833e5e6e61f4a9a4343808509e3bc119093
MalikaSadullaeva96 <m.sadullaeva96@gmail.com>
2023-06-18 23:35:21 +0500

    feat: function getMovingSum

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -266,8 +266,15 @@ function toArrayOfSquares(arr) {
  *   [ 0, 0, 0, 0, 0]         => [ 0, 0, 0, 0, 0]
  *   [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ] => [ 1, 3, 6, 10, 15, 21, 28, 36, 45, 55 ]
  */
-function getMovingSum(/* arr */) {
-  throw new Error('Not implemented');
+function getMovingSum(arr) {
+  return arr.reduce((acc, curr, index) => {
+    if (index === 0) {
+      acc.push(curr);
+    } else {
+      acc.push(acc[index - 1] + curr);
+    }
+    return acc;
+  }, []);
 }
 
 /**
