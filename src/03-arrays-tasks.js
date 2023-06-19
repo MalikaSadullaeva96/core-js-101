b0b38da0df7a102924efda9c0f7d2ab0bb959221
MalikaSadullaeva96 <m.sadullaeva96@gmail.com>
2023-06-19 23:08:01 +0500

    feat: array uniq

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -536,8 +536,8 @@ function getIntervalArray(start, end) {
  *   [ 'a', 'a', 'a', 'a' ]  => [ 'a' ]
  *   [ 1, 1, 2, 2, 3, 3, 4, 4] => [ 1, 2, 3, 4]
  */
-function distinct(/* arr */) {
-  throw new Error('Not implemented');
+function distinct(arr) {
+  return arr.filter((val, idx) => idx === arr.indexOf(val));
 }
 
 /**
